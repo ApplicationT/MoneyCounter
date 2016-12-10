d48f786732a84156d6297dc9d57d2876e2d77986
Xiamin <610315802@qq.com>
2016-12-11 01:18:25 +0800

    修改主界面

---
+++ app/src/main/java/applock/anderson/com/moneycounter/MainActivity.java
@@ -0,0 +1,17 @@
+package applock.anderson.com.moneycounter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Xiamin on 2016/12/11.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
--- app/src/main/java/applock/anderson/com/moneycounter/WelcomeActivity.java
+++ app/src/main/java/applock/anderson/com/moneycounter/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package applock.anderson.com.moneycounter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,6 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
     }
 }
