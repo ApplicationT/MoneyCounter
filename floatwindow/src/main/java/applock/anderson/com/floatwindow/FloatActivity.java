17d7620f4b2546f75d90f2bd7a32439b1353d7aa
Xiamin <610315802@qq.com>
2016-12-11 00:49:59 +0800

    增加浮悬窗module，修改gradle版本为2.2.3

---
+++ floatwindow/src/main/java/applock/anderson/com/floatwindow/FloatActivity.java
@@ -0,0 +1,13 @@
+package applock.anderson.com.floatwindow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FloatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_float);
+    }
+}
